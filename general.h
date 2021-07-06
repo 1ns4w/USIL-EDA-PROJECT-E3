ca2b194a23b5d3325ee8600fb7a9e2b866796683
Kevin De Lama <74796011+KevinDeLama2020@users.noreply.github.com>
2021-07-05 21:25:38 -0500

    Subiendo general.h
    Kevin De Lama Huaccho

---
+++ general.h
@@ -0,0 +1,11 @@
+#ifndef __GENERAL_H__
+#define __GENERAL_H__
+
+#include "type.h"
+#include "MyExc.h"
+#include "LexicalAnalyzer.h"
+#include "NodeNumber.h"
+#include "NodeOperation.h"
+#include "NodeVariable.h"
+
+#endif
