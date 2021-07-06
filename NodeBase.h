395ad4c45006a09d1028581fa06a944b4b6e2581
Kevin De Lama <74796011+KevinDeLama2020@users.noreply.github.com>
2021-07-05 21:19:28 -0500

    Subiendo NodeBase.h
    Kevin De Lama Huaccho

---
+++ NodeBase.h
@@ -0,0 +1,21 @@
+#ifndef __NODE_BASE_H__
+#define __NODE_BASE_H__
+
+#include "type.h"
+#include "cassert"
+using namespace std;
+
+class NodeBase
+{
+  protected:
+    TX m_value = 0;
+    
+    TX getValue() { return m_value; }
+    virtual void setValue(TX value) = 0;
+  public:
+    NodeBase(TX val = 0) : m_value(val) {};
+    
+    virtual TX eval() = 0;
+
+};
+#endif
