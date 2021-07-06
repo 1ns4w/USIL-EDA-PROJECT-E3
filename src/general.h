cf18552df1c36c522e8bbec8e24801dd380c1d2f
Maizo Diego <72949835+MaizoDiego@users.noreply.github.com>
2021-07-05 21:35:18 -0500

    Creación del general.h Maizo

---
+++ src/general.h
@@ -0,0 +1,11 @@
+#ifndef __GENERAL_H__
+#define __GENERAL_H__
+
+#include "type.h"
+#include "myExc.h"
+#include "LexicalAnalyzer.h"
+#include "NodeNumber.h"
+#include "NodeOperation.h"
+#include "NodeVariable.h"
+
+#endif
