78ae86574e70fcbbf29a75d3bf0b91c6e85e676f
Maizo Diego <72949835+MaizoDiego@users.noreply.github.com>
2021-07-05 21:33:44 -0500

    Main.cpp Maizo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-using namespace std;
+#include "general.h"
 
 int main()
 {
-  cout << "Hello, world!";
-  return 0;
+  LexicalAnalyzer la;
+  //string exp = "a+b/(35^X-(y+3))-6";
+  //string exp = "a+b/x-y+(3-6)-(x+y)-3";
+  string exp = "2+1/6-4";
+  try
+  {
+    NodeBase *pExp = la.Analize(exp);
+
+  }
+  catch(MyException *pExc)
+  {
+    cerr << pExc->what() << endl;
+  }
 }
