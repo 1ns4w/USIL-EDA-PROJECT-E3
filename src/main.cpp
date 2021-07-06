c6f7912ad8449c328f74404851346eee11928be7
Jose Terrones <66368520+RadeonXYZ@users.noreply.github.com>
2021-07-05 22:17:40 -0500

    Main.cpp-Terrones
    by Jose Terrones

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-using namespace std;
+#include "general.h"
 
 int main()
 {
-  cout << "Hello, world!";
-  return 0;
+  LexicalAnalyzer la;
+  string exp = "(a+b/x)-y+18/(3-6)";
+  try
+  {
+    NodeBase *pExp = la.Analyze(exp);
+
+    list<string> tokens = la.getListofTokens(exp);
+    
+    cout << "imput: " << exp << endl;
+    for(auto tok: tokens)
+      cout << tok << endl;
+  }
+  catch(MyException *pExc)
+  {
+    cerr << pExc->what() << endl;
+  }
 }
